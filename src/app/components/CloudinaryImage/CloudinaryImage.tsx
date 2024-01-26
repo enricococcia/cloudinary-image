d03a485e8ec94a2d85895575ff21efdb943b42b6
Elisa Marangon <elisa.marangon@deltatre.com>
2024-01-26 16:03:17 +0100

    add private signed delivery urls

--- src/app/components/CloudinaryImage/CloudinaryImage.tsx
+++ src/app/components/CloudinaryImage/CloudinaryImage.tsx
@@ -1,11 +1,20 @@
-import cloudinary from '@/app/cloudinary';
+import cloudinary from "@/app/cloudinary";
 
 const CloudinaryImage = () => {
-  const cloudinaryImage = cloudinary.image('brown_sheep.jpg', {
+  const cloudinaryImage = cloudinary.image("dev/wkzhgskc5jruztddyhki", {
+    // transformation: [
+    //   { height: 200, width: 300, x: 355, y: 410, crop: "crop" },
+    //   { height: 100, width: 130, crop: "fill" },
+    // ],
     transformation: [
-      { height: 200, width: 300, x: 355, y: 410, crop: 'crop' },
-      { height: 100, width: 130, crop: 'fill' },
+      { effect: "cartoonify" },
+      { radius: "max" },
+      { effect: "outline:100", color: "lightblue" },
+      { background: "lightblue" },
+      { height: 300, crop: "scale" },
     ],
+    sign_url: true,
+    type: "private",
   });
 
   return <div dangerouslySetInnerHTML={{ __html: cloudinaryImage }} />;
